ed8875625cb05846a3a9099d120c09ca645d0c24
Pawan Sharma <pawansharma93520@gmail.com>
2024-02-29 01:49:04 +0530

    project starting file

---
+++ index.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+document.querySelectorAll("button")[].addEventListener("click", function () {
+  alert("I got clicked");
+});
+}
